75d8781f10a10be1ad6408f5721a883c74e5b254
shawntai <shawn890715@gmail.com>
2023-03-01 22:17:26 -0500

    Part 1

--- grammar.py
+++ grammar.py
@@ -6,7 +6,7 @@ Daniel Bauer
 
 import sys
 from collections import defaultdict
-from math import fsum
+from math import fsum, isclose
 
 
 class Pcfg(object):
@@ -47,9 +47,36 @@ class Pcfg(object):
         Otherwise return False.
         """
         # TODO, Part 1
-        return False
+        for nonterminal in self.lhs_to_rules:
+            rules = self.lhs_to_rules[nonterminal]
+            prob_sum = fsum([rule[2] for rule in rules])
+            if not isclose(prob_sum, 1.0):
+                print(f"Sum of probabilities for {nonterminal} is {prob_sum}")
+                return False
+            for rule in rules:
+                if len(rule[1]) == 1 and rule[1][0] in self.lhs_to_rules:
+                    print(f"Rule {rule} has a nonterminal on the right hand side.")
+                    # print(f"Rule {rule} has a terminal on the right hand side.")
+                    return False
+                if len(rule[1]) == 2 and (
+                    rule[1][0] not in self.lhs_to_rules
+                    or rule[1][1] not in self.lhs_to_rules
+                ):
+                    print(f"Rule {rule} has a terminal on the right hand side.")
+                    return False
+                if len(rule[1]) > 2:
+                    print(
+                        f"Rule {rule} has more than two symbols on the right hand side."
+                    )
+                    return False
+        if self.startsymbol not in self.lhs_to_rules:
+            print(f"Start symbol {self.startsymbol} is not in the grammar.")
+            return False
+        return True
 
 
 if __name__ == "__main__":
-    with open(sys.argv[1], "r") as grammar_file:
+    # with open(sys.argv[1], "r") as grammar_file:
+    with open("atis3.pcfg", "r") as grammar_file:
         grammar = Pcfg(grammar_file)
+        print(grammar.verify_grammar())
